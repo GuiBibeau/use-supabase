1f8e6ac989b3c42dfed6fa8cfd4740a846f601bf
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2021-02-24 16:21:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Use Supabase
+
+This is a simple zero dependencies package that gives you access to your Supabase client using a React hook!
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './styles.scss'
+
+const Package: React.FC = () => (
+  <div className="package">
+    <h2>Do cool stuff</h2>
+  </div>
+)
+
+export default Package
