4610f3d924da0f88d94afc6692c2a30b61bf2d93
Luca Montaigut <luca.montaigut@uptime.ac>
2022-03-25 17:28:01 +0100

    add refetch on useQuery

--- src/hooks/useQuery.tsx
+++ src/hooks/useQuery.tsx
@@ -2,36 +2,44 @@ import { useEffect, useState } from 'react'
 import type { Response } from '../types'
 import { PostgrestBuilder } from '@supabase/postgrest-js'
 
-export const useQuery = <T,>(query: PostgrestBuilder<T>) => {
+export const useQuery = <T,>(
+  query: PostgrestBuilder<T>
+): Response<T> & {
+  refetch: () => Promise<void>
+} => {
   const [queryState, setQueryState] = useState<Response<T>>({
     data: null,
     loading: true,
     error: null,
   })
 
+  const fetchData = async () => {
+    try {
+      const { data, error } = await query
+      if (error) throw new Error(`Error ${error.code}: ${error.message}`)
+      setQueryState((prev) =>
+        prev !== null
+          ? { ...prev, data, loading: false, error: null }
+          : { data, loading: false, error: null }
+      )
+    } catch (error) {
+      if (error instanceof Error) {
+        setQueryState((prev) => ({
+          ...prev,
+          data: null,
+          loading: false,
+          error: error as Error,
+        }))
+      }
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
     ;(async () => {
-      try {
-        const { data, error } = await query
-        if (error) throw new Error(`Error ${error.code}: ${error.message}`)
-        setQueryState((prev) =>
-          prev !== null
-            ? { ...prev, data, loading: false, error: null }
-            : { data, loading: false, error: null }
-        )
-      } catch (error) {
-        if (error instanceof Error) {
-          setQueryState((prev) => ({
-            ...prev,
-            data: null,
-            loading: false,
-            error: error as Error,
-          }))
-        }
-        console.error(error)
-      }
+      fetchData()
     })()
   }, [])
 
-  return queryState
+  return { ...queryState, refetch: fetchData }
 }
